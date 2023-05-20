ed670f2130c551a36fa6398b84089280e498be93
wakamenod <wakamenod@gmail.com>
2023-05-20 20:29:17 +0900

    abc101

---
+++ abc101/a/main.rb
@@ -0,0 +1,9 @@
+# [A] Eating Symbols Easy
+# https://atcoder.jp/contests/abc101/tasks/abc101_a
+# $stdin = DATA
+# S = gets
+# p gets.split.take(n).collect(&:to_i)
+p 4 - gets.count('-') * 2
+__END__
+3
+10 20 30
