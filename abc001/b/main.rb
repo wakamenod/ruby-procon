4b155e0ef0bd5c04efc3e8f8b12ad94da3e49045
wakamenod <wakamenod@gmail.com>
2023-11-10 08:59:52 +0900

    B001, A013

--- abc001/b/main.rb
+++ abc001/b/main.rb
@@ -1,6 +1,18 @@
-$stdin = DATA
+## [B] 視程の通報
+## https://atcoder.jp/contests/abc001/tasks/abc001_2
 n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+
+if n < 100
+  puts '00'
+elsif n <= 5000
+  # 距離（メートル）をキロメートルに変換し、10倍して2桁で出力
+  puts format('%02d', n / 100)
+elsif n <= 30000
+  # キロメートル単位に変換し、50を足す
+  puts format('%02d', n / 1000 + 50)
+elsif n <= 70000
+  # キロメートル単位に変換し、30を引いて5で割り、80を足す
+  puts format('%02d', (n / 1000 - 30) / 5 + 80)
+else
+  puts '89'
+end
