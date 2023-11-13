45a10ab6973338f65196be9eb433e82653eb9b6b
wakamenod <wakamenod@gmail.com>
2023-11-14 08:52:37 +0900

    A021, B010

--- abc010/b/main.rb
+++ abc010/b/main.rb
@@ -1,6 +1,16 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] 花占い
+## https://atcoder.jp/contests/abc010/tasks/abc010_2
+gets.to_i
+
+sum = 0
+gets.split.map(&:to_i).each do |a|
+  if (a % 6).zero?
+    sum += 3
+  elsif a % 3 == 2 && a.odd?
+    sum += 2
+  elsif a.even?
+    sum += 1
+  end
+end
+
+puts sum
