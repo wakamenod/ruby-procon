1f6a0154b8e64a3c4a52e691d96f1171f28475b7
wakamenod <wakamenod@gmail.com>
2023-11-19 09:32:49 +0900

    A037, B023

--- abc023/b/main.rb
+++ abc023/b/main.rb
@@ -1,6 +1,14 @@
-$stdin = DATA
+## [B] 手芸王
+## https://atcoder.jp/contests/abc023/tasks/abc023_b
+
+def f(n, i, s)
+  return s if s.size >= n
+
+  ac = i % 3 == 1 ? "a#{s}c" : i % 3 == 2 ? "c#{s}a" : "b#{s}b"
+  f(n, i + 1, ac)
+end
+
 n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+s = gets.chomp
+ans = f(n, 1, 'b')
+puts s == ans ? n / 2 : -1
