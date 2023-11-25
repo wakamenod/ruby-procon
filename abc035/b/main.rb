6ed092bbd587293c6243672a1a20700651e28e42
wakamenod <wakamenod@gmail.com>
2023-11-25 09:09:39 +0900

    A062, B035

--- abc035/b/main.rb
+++ abc035/b/main.rb
@@ -1,6 +1,28 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] ドローン
+## https://atcoder.jp/contests/abc035/tasks/abc035_b
+s = gets.chomp
+t = gets.to_i
+
+x = 0
+y = 0
+h = 0
+s.chars.each do |c|
+  case c
+  when 'L'
+    x -= 1
+  when 'R'
+    x += 1
+  when 'U'
+    y += 1
+  when 'D'
+    y -= 1
+  when '?'
+    h += 1
+  end
+end
+
+if t == 1
+  puts x.abs + y.abs + h
+else
+  puts [x.abs + y.abs - h, (x.abs + y.abs - h).even? ? 0 : 1].max
+end
