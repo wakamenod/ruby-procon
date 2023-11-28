dd8731893e3113ab99bab1205d72f58d17f295b8
wakamenod <wakamenod@gmail.com>
2023-11-29 05:30:16 +0900

    A086, B047

--- abc046/b/main.rb
+++ abc046/b/main.rb
@@ -1,6 +1,4 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Painting Balls with AtCoDeer
+## https://atcoder.jp/contests/abc046/tasks/abc046_b
+n, k = gets.split.map(&:to_i)
+puts k * ((k - 1)**(n - 1))
--- abc047/b/main.rb
+++ abc047/b/main.rb
@@ -1,6 +1,21 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Snuke's Coloring 2 (ABC Edit)
+## https://atcoder.jp/contests/abc047/tasks/abc047_b
+w, h, n = gets.split.map(&:to_i)
+maxw = w
+minw = 0
+maxh = h
+minh = 0
+(0...n).each do
+  x, y, a = gets.split.map(&:to_i)
+  case a
+  when 1
+    minw = [minw, x].max
+  when 2
+    maxw = [maxw, x].min
+  when 3
+    minh = [minh, y].max
+  when 4
+    maxh = [maxh, y].min
+  end
+end
+puts [maxh - minh, 0].max * [maxw - minw, 0].max
