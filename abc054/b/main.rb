0777184be4d7c4c1aa7aebafb44d2c6f8e1e5e07
wakamenod <wakamenod@gmail.com>
2023-11-30 07:48:27 +0900

    A094, B054

--- abc054/b/main.rb
+++ abc054/b/main.rb
@@ -1,6 +1,18 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Template Matching
+## https://atcoder.jp/contests/abc054/tasks/abc054_b
+n, m = gets.split.map(&:to_i)
+as = (0...n).map { gets.chomp }
+bs = (0...m).map { gets.chomp }
+(0...n - m + 1).each do |i|
+  (0...n - m + 1).each do |j|
+    found = true
+    bs.each_with_index do |brow, k|
+      found = false unless as[i + k][j, brow.length] == brow
+    end
+    if found
+      puts 'Yes'
+      exit
+    end
+  end
+end
+puts 'No'
