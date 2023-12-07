374f55d65104530cad24655b42dd374f827f5bcd
wakamenod <wakamenod@gmail.com>
2023-12-07 09:04:24 +0900

    A122, B075

--- abc075/b/main.rb
+++ abc075/b/main.rb
@@ -1,6 +1,21 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Minesweeper
+## https://atcoder.jp/contests/abc075/tasks/abc075_b
+def cnt(h, w, i, j, s)
+  offsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+  offsets.sum do |di, dj|
+    ni = i + di
+    nj = j + dj
+    ni.between?(0, h - 1) && nj.between?(0, w - 1) && s[ni][nj] == '#' ? 1 : 0
+  end
+end
+
+h, w = gets.split.map(&:to_i)
+s = (1..h).map { gets.chomp }
+
+(0...h).each do |i|
+  (0...w).each do |j|
+    print s[i][j] == '#' ? '#' : cnt(h, w, i, j, s)
+  end
+  puts
+end
