e17a6100920a558bfd6ec0f7759965c3dd69c636
wakamenod <wakamenod@gmail.com>
2023-12-10 22:33:41 +0900

    typical001

--- typical90/001/main.rb
+++ typical90/001/main.rb
@@ -1,8 +1,30 @@
 ## [001] Yokan Party（★4）
 ## https://atcoder.jp/contests/typical90/tasks/typical90_a
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+_, l = gets.split.map(&:to_i)
+k = gets.to_i
+as = gets.split.map(&:to_i)
+
+check = lambda do |x|
+  cnt = 0
+  pre = 0
+  as.each do |a|
+    if a - pre >= x
+      cnt += 1
+      pre = a
+    end
+  end
+  cnt += 1 if l - pre >= x
+  return cnt >= k + 1
+end
+
+left = -1
+right = l + 1
+while right - left > 1
+  mid = (right + left) / 2
+  if check.call(mid)
+    left = mid
+  else
+    right = mid
+  end
+end
+puts left
