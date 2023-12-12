e2a8b33b2ccbf2597e4655a7445fd5db98c8a22b
wakamenod <wakamenod@gmail.com>
2023-12-12 09:57:56 +0900

    typical002

--- typical90/002/main.rb
+++ typical90/002/main.rb
@@ -1,6 +1,23 @@
-$stdin = DATA
+## [002] Encyclopedia of Parentheses（★3）
+## https://atcoder.jp/contests/typical90/tasks/typical90_b
+
+def valid(s)
+  score = 0
+  s.each do |c|
+    if c == '('
+      score += 1
+    else
+      score -= 1
+    end
+    return false if score < 0
+  end
+  score == 0
+end
+
 n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+list = ['(', ')'].repeated_permutation(n).to_a
+ans = ''
+list.each do |k|
+  ans << "#{k.join}\n" if valid(k)
+end
+puts ans
