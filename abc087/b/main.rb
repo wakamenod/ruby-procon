e73fd427b29a55c33891e2777ebaa9333a83c9c8
wakamenod <wakamenod@gmail.com>
2023-12-15 23:17:28 +0900

    B087

--- abc087/b/main.rb
+++ abc087/b/main.rb
@@ -1,6 +1,16 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Coins
+## https://atcoder.jp/contests/abc087/tasks/abc087_b
+a = gets.to_i
+b = gets.to_i
+c = gets.to_i
+x = gets.to_i
+sum = 0
+a.downto(0) do |ai|
+  b.downto(0) do |bi|
+    c.downto(0) do |ci|
+      sum += 1 if ai * 500 + bi * 100 + ci * 50 == x
+    end
+  end
+end
+
+puts sum
