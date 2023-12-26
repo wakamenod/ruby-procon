cee67009d5eb2e47596b752002511db19950c993
wakamenod <wakamenod@gmail.com>
2023-12-26 09:06:34 +0900

    B106

--- abc106/b/main.rb
+++ abc106/b/main.rb
@@ -1,6 +1,23 @@
-$stdin = DATA
+## [B] 105
+## https://atcoder.jp/contests/abc106/tasks/abc106_b
 n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+
+def yaku(n)
+  divisors = []
+  (1..Math.sqrt(n).to_i).each do |i|
+    if n % i == 0
+      divisors.push(i)
+      divisors.push(n / i) unless i * i == n
+    end
+  end
+  divisors.sort
+end
+
+ans = 0
+i = 1
+while i <= n
+  ans += 1 if yaku(i).length == 8
+  i += 2
+end
+
+puts ans
--- typical90/006/___main.rb
+++ typical90/006/___main.rb
@@ -1,8 +1,5 @@
 ## [006] Smallest Subsequence（★5）
 ## https://atcoder.jp/contests/typical90/tasks/typical90_f
-N, K = gets.split.map(&:to_i)
-S = gets.chomp
-
 def nex(s)
   n = s.length
   res = Array.new(n + 1) { Array.new(26, n) }
@@ -21,19 +18,23 @@ def nex(s)
   res
 end
 
+N, K = gets.split.map(&:to_i)
+S = gets.chomp
+
 nex = nex(S)
 
 res = ''
 j = -1
-K.times do |i|
-  26.times do |chi|
-    k = nex[j + 1][chi]
+K.times do
+  26.times do |ordc|
+    k = nex[j + 1][ordc]
 
-    next unless N - k >= K - i
+    next unless N - k >= K - res.size
 
-    res << ('a'.ord + chi).chr
+    res << ('a'.ord + ordc).chr
     j = k
     break
   end
 end
+
 puts res
