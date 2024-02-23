4a8f8f4294338464a833a27a3ac0fce406aa0482
wakamenod <wakamenod@gmail.com>
2024-02-23 11:32:41 +0900

    B250

--- abc250/b/main.rb
+++ abc250/b/main.rb
@@ -1,6 +1,14 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Enlarged Checker Board
+## https://atcoder.jp/contests/abc250/tasks/abc250_b
+n, a, b = gets.split.map(&:to_i)
+g = Array.new(a * n) { Array.new(b * n) }
+n.times do |i|
+  n.times do |j|
+    a.times do |y|
+      b.times do |x|
+        g[y + i * a][x + j * b] = (i + j) % 2 == 0 ? '.' : '#'
+      end
+    end
+  end
+end
+puts g.each.map(&:join)
