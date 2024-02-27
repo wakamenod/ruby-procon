a3593351d2e76c903212f4a466b5c152c9d64355
wakamenod <wakamenod@gmail.com>
2024-02-27 09:23:48 +0900

    B260

--- abc260/b/main.rb
+++ abc260/b/main.rb
@@ -1,6 +1,12 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Better Students Are Needed!
+## https://atcoder.jp/contests/abc260/tasks/abc260_b
+n, x, y, z = gets.split.map(&:to_i)
+I = (1..n).to_a
+A = gets.split.map(&:to_i)
+B = gets.split.map(&:to_i)
+AB = [I, A, B].transpose
+m = 10_000
+s = AB.sort_by { |i, a, _b| (100 - a) * m + i }.map(&:first).take(x).to_set
+s += AB.sort_by { |i, _a, b| (100 - b) * m + i }.reject { |k| s.include?(k[0]) }.map(&:first).take(y).to_set
+s += AB.sort_by { |i, a, b| (200 - (a + b)) * m + i }.reject { |k| s.include?(k[0]) }.map(&:first).take(z).to_set
+puts s.to_a.sort
