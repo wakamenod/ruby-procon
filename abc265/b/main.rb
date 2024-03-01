8f3552fb347cee081e183150eedfeb2dba242621
wakamenod <wakamenod@gmail.com>
2024-03-01 09:11:13 +0900

    B265

--- abc265/b/main.rb
+++ abc265/b/main.rb
@@ -1,6 +1,20 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Explore
+## https://atcoder.jp/contests/abc265/tasks/abc265_b
+n, m, t = gets.split.map(&:to_i)
+A = gets.split.map(&:to_i)
+b = Array.new(n + 5, 0)
+m.times do
+  x, y = gets.split.map(&:to_i)
+  b[x] = y
+end
+
+ans = 'Yes'
+A.each_with_index do |a, i|
+  t -= a
+  if t <= 0
+    ans = 'No'
+    break
+  end
+  t += b[i + 2]
+end
+puts ans
