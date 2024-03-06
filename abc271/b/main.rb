93b7830a4b7dbf8c26babce06dc99b499ac67fe3
wakamenod <wakamenod@gmail.com>
2024-03-07 08:00:29 +0900

    B271

--- abc271/b/main.rb
+++ abc271/b/main.rb
@@ -1,6 +1,8 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Maintain Multiple Sequences
+## https://atcoder.jp/contests/abc271/tasks/abc271_b
+n, q = gets.split.map(&:to_i)
+a = n.times.map { gets.split.map(&:to_i) }
+q.times do
+  s, t = gets.split.map(&:to_i)
+  puts a[s - 1][t]
+end
