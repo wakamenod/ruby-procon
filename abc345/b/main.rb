d266f933ce91a2875dc5917f29cf302c89140a52
wakamenod <wakamenod@gmail.com>
2024-03-28 08:39:28 +0900

    B345

--- abc345/b/main.rb
+++ abc345/b/main.rb
@@ -1,6 +1,3 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Integer Division Returns
+## https://atcoder.jp/contests/abc345/tasks/abc345_b
+puts (gets.to_i + 9) / 10
