d3e7b3592c1e6558b7285d7f1f50681bafa157d3
wakamenod <wakamenod@gmail.com>
2024-04-01 08:51:58 +0900

    B347

--- abc347/b/main.rb
+++ abc347/b/main.rb
@@ -1,6 +1,10 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Substring
+## https://atcoder.jp/contests/abc347/tasks/abc347_b
+S = gets.chomp
+s = Set.new
+0.upto(S.length).each do |i|
+  i.upto(S.length) do |j|
+    s.add(S[i...j]) unless S[i...j].empty?
+  end
+end
+puts s.length
