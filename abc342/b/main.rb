1c8c31f82b9edfe90c2eeb8214251c755ceff980
wakamenod <wakamenod@gmail.com>
2024-04-04 09:19:50 +0900

    B342

--- abc342/b/main.rb
+++ abc342/b/main.rb
@@ -1,6 +1,11 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Which is ahead?
+## https://atcoder.jp/contests/abc342/tasks/abc342_b
+N = gets.to_i
+P = gets.split.map(&:to_i)
+Q = gets.to_i
+Q.times do
+  a, b = gets.split.map(&:to_i)
+  ai = P.find_index(a)
+  bi = P.find_index(b)
+  puts ai < bi ? a : b
+end
