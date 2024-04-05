2ef90b38c6cb6fc1c726212ffa5ec6d0f93c1f3c
wakamenod <wakamenod@gmail.com>
2024-04-05 23:55:17 +0900

    340B

--- abc340/b/main.rb
+++ abc340/b/main.rb
@@ -1,6 +1,12 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Append
+## https://atcoder.jp/contests/abc340/tasks/abc340_b
+Q = gets.to_i
+ar = []
+Q.times do
+  q, a = gets.split.map(&:to_i)
+  if q == 1
+    ar << a
+  else
+    puts ar[-a]
+  end
+end
