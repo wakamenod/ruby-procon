9dcadd2b4322719e82bd77c4447e0c6605714898
wakamenod <wakamenod@gmail.com>
2024-04-06 21:46:54 +0900

    B339

--- abc339/b/main.rb
+++ abc339/b/main.rb
@@ -1,6 +1,27 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Langton's Takahashi
+## https://atcoder.jp/contests/abc339/tasks/abc339_b
+H, W, N = gets.split.map(&:to_i)
+g = Array.new(H) { '.' * W }
+ch = 0
+cw = 0
+d = 0
+N.times do
+  if g[ch % H][cw % W] == '.'
+    g[ch % H][cw % W] = '#'
+    d += 1
+  else
+    g[ch % H][cw % W] = '.'
+    d -= 1
+  end
+  case d % 4
+  when 0
+    ch -= 1
+  when 1
+    cw += 1
+  when 2
+    ch += 1
+  when 3
+    cw -= 1
+  end
+end
+puts g
