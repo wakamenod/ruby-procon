322ccafcfddb76548abb0a57a1ef58280d989d70
wakamenod <wakamenod@gmail.com>
2024-04-09 07:30:23 +0900

    B337

--- abc337/b/main.rb
+++ abc337/b/main.rb
@@ -1,6 +1,12 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Extended ABC
+## https://atcoder.jp/contests/abc337/tasks/abc337_b
+s = gets.chomp
+sc = ''
+ans = ''
+s.chars.each do |c|
+  if sc != c
+    sc = c
+    ans += c
+  end
+end
+puts ans == 'AC' || 'ABC'.include?(ans) ? 'Yes' : 'No'
