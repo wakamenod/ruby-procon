87199bcb2e5158b1b98ac862e14abadb487f1055
wakamenod <wakamenod@gmail.com>
2024-04-12 06:55:11 +0900

    B332

--- abc332/b/main.rb
+++ abc332/b/main.rb
@@ -1,6 +1,17 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Glass and Mug
+## https://atcoder.jp/contests/abc332/tasks/abc332_b
+g = 0
+m = 0
+K, G, M = gets.split.map(&:to_i)
+K.times do
+  if g == G
+    g = 0
+  elsif m == 0
+    m = M
+  else
+    s = [G - g, m].min
+    g += s
+    m -= s
+  end
+end
+puts [g, m].join(' ')
