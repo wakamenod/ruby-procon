ffed729ae8f88c01b52f5344506a186a66762f91
wakamenod <wakamenod@gmail.com>
2024-04-13 06:39:58 +0900

    B328

--- abc328/b/main.rb
+++ abc328/b/main.rb
@@ -1,6 +1,11 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] 11/11
+## https://atcoder.jp/contests/abc328/tasks/abc328_b
+N = gets.to_i
+D = gets.split.map(&:to_i)
+ans = 0
+N.times do |i|
+  D[i].times do |j|
+    ans += 1 if "#{i + 1}#{j + 1}".chars.to_set.length == 1
+  end
+end
+puts ans
