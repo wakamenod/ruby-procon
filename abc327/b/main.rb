3d3386960aa0e888071094aeaa73e0fa962c07b6
wakamenod <wakamenod@gmail.com>
2024-04-14 06:48:05 +0900

    B327

--- abc327/b/main.rb
+++ abc327/b/main.rb
@@ -1,6 +1,10 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] A^A
+## https://atcoder.jp/contests/abc327/tasks/abc327_b
+B = gets.to_i
+1.upto(100) do |i|
+  if i**i == B
+    puts i
+    exit
+  end
+end
+puts(-1)
