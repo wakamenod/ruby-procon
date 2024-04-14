241710c7dbacfa4d272cdf795609f71119a4f3e3
wakamenod <wakamenod@gmail.com>
2024-04-15 07:23:00 +0900

    B324

--- abc324/b/main.rb
+++ abc324/b/main.rb
@@ -1,6 +1,12 @@
-$stdin = DATA
+## [B] 3-smooth Numbers
+## https://atcoder.jp/contests/abc324/tasks/abc324_b
 n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+100.times do |i|
+  100.times do |j|
+    if 2**i * 3**j == n
+      puts 'Yes'
+      exit
+    end
+  end
+end
+puts 'No'
