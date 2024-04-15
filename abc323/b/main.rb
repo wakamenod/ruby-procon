9a2dc9a6cb318aeee42abe9bcc64196ee632aab5
wakamenod <wakamenod@gmail.com>
2024-04-16 07:09:02 +0900

    B323

--- abc323/b/main.rb
+++ abc323/b/main.rb
@@ -1,6 +1,5 @@
-$stdin = DATA
+## [B] Round-Robin Tournament
+## https://atcoder.jp/contests/abc323/tasks/abc323_b
 n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+ms = n.times.map { gets.chars.filter { |v| v == 'o' }.length }.map.with_index(1)
+puts(ms.sort_by { |v, k| v * 1000 + (n - k) }.map { |_v, k| k }.reverse.join(' '))
