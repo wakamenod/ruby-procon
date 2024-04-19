c2302f9fd4987872396236dad644a2673d5b52df
wakamenod <wakamenod@gmail.com>
2024-04-20 07:18:02 +0900

    B319, B318

--- abc318/b/main.rb
+++ abc318/b/main.rb
@@ -1,6 +1,13 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Overlapping sheets
+## https://atcoder.jp/contests/abc318/tasks/abc318_b
+N = gets.to_i
+s = Set.new
+N.times do
+  a, b, c, d = gets.split.map(&:to_i)
+  a.upto(b - 1) do |x|
+    c.upto(d - 1) do |y|
+      s.add("x#{x}:y#{y}")
+    end
+  end
+end
+puts s.length
--- abc319/b/main.rb
+++ abc319/b/main.rb
@@ -1,6 +1,10 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Measure
+## https://atcoder.jp/contests/abc319/tasks/abc319_b
+N = gets.to_i
+js = (1..9).filter { |j| N % j == 0 }
+ans = []
+(N + 1).times do |i|
+  j = js.find { |a| i % (N / a) == 0 }
+  ans << (j.nil? ? '-' : j.to_s)
+end
+puts ans.join
