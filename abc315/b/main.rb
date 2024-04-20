aa78ec07380e81d11e03b57b637e686a1caedaa8
wakamenod <wakamenod@gmail.com>
2024-04-21 07:05:30 +0900

    B315

--- abc315/b/main.rb
+++ abc315/b/main.rb
@@ -1,6 +1,14 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] The Middle Day
+## https://atcoder.jp/contests/abc315/tasks/abc315_b
+M = gets.to_i
+D = gets.split.map(&:to_i)
+mid = (D.sum + 1) / 2
+sum = 0
+D.each_with_index do |d, i|
+  sum += d
+  next unless sum >= mid
+
+  diff = sum - mid
+  puts "#{i + 1} #{d - diff}"
+  break
+end
