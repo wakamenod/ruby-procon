bb7cb7f960b9d0528aa6cfdbf8a0f6831deb2979
wakamenod <wakamenod@gmail.com>
2024-04-22 07:07:29 +0900

    B311

--- abc311/b/main.rb
+++ abc311/b/main.rb
@@ -1,6 +1,15 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Vacation Together
+## https://atcoder.jp/contests/abc311/tasks/abc311_b
+N, D = gets.split.map(&:to_i)
+S = N.times.map { gets.chomp.chars }.transpose
+max = 0
+c = 0
+S.each do |s|
+  if s.include?('x')
+    c = 0
+  else
+    c += 1
+    max = [c, max].max
+  end
+end
+puts max
