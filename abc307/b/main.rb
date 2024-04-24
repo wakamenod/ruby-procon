b4837cbd5230ffcb5bb763a8bc1f5dcef9e3c657
wakamenod <wakamenod@gmail.com>
2024-04-25 06:44:46 +0900

    B307

--- abc307/b/main.rb
+++ abc307/b/main.rb
@@ -1,6 +1,16 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] racecar
+## https://atcoder.jp/contests/abc307/tasks/abc307_b
+N = gets.to_i
+S = N.times.map { gets.chomp }
+0.upto(N - 1) do |i|
+  0.upto(N - 1) do |j|
+    next if i == j
+
+    s = "#{S[i]}#{S[j]}"
+    if s == s.reverse
+      puts 'Yes'
+      exit
+    end
+  end
+end
+puts 'No'
