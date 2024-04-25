cff78d849515151814b9044635cc335c61372bcb
wakamenod <wakamenod@gmail.com>
2024-04-26 07:08:28 +0900

    B305

--- abc305/b/main.rb
+++ abc305/b/main.rb
@@ -1,6 +1,15 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] ABCDEFG
+## https://atcoder.jp/contests/abc305/tasks/abc305_b
+P, Q = gets.split.sort.map(&:ord)
+ans = 0
+(P + 1..Q).map(&:chr).each do |a|
+  ans += case a
+         when 'B' then 3
+         when 'C' then 1
+         when 'D' then 4
+         when 'E' then 1
+         when 'F' then 5
+         when 'G' then 9
+         end
+end
+puts ans
