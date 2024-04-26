f48d9b0c3b0cf44009e903fc0b2489bbebb140d6
wakamenod <wakamenod@gmail.com>
2024-04-27 06:40:00 +0900

    B301

--- abc301/b/main.rb
+++ abc301/b/main.rb
@@ -1,6 +1,10 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B]  Fill the Gaps
+## https://atcoder.jp/contests/abc301/tasks/abc301_b
+N = gets.to_i
+A = gets.split.map(&:to_i)
+ans = []
+A.each_cons(2) do |s, g|
+  a = s > g ? s.downto(g) : s.upto(g)
+  ans << (ans.empty? ? a.to_a : a.to_a[1..])
+end
+puts ans.join(' ')
