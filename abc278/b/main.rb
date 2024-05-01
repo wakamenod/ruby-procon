00c03ff8d840617de9de10eca2c8fb8cc78565a6
wakamenod <wakamenod@gmail.com>
2024-05-02 07:06:18 +0900

    B278

--- abc278/b/main.rb
+++ abc278/b/main.rb
@@ -1,6 +1,14 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Misjudge the Time
+## https://atcoder.jp/contests/abc278/tasks/abc278_b
+H, M = gets.split.map(&:to_i)
+m = H * 60 + M
+
+loop do
+  r = m % (24 * 60)
+  s = "#{(r / 60).to_s.rjust(2, '0')}#{(r % 60).to_s.rjust(2, '0')}"
+  if (s[0] + s[2]).to_i < 24 && (s[1] + s[3]).to_i < 60
+    puts "#{s[0..1]} #{s[2..]}"
+    break
+  end
+  m += 1
+end
