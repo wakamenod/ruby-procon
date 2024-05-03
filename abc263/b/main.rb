5e0bcc484fc28baa7224266551d86e83842d5999
wakamenod <wakamenod@gmail.com>
2024-05-03 16:54:44 +0900

    B263, B270

--- abc263/b/main.rb
+++ abc263/b/main.rb
@@ -1,6 +1,11 @@
-$stdin = DATA
+## [B] Ancestor
+## https://atcoder.jp/contests/abc263/tasks/abc263_b
 n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+P = gets.split.map(&:to_i)
+c = n
+ans = 0
+while c != 1
+  ans += 1
+  c = P[c - 2]
+end
+puts ans
--- abc270/b/main.rb
+++ abc270/b/main.rb
@@ -1,6 +1,19 @@
-$stdin = DATA
-n = gets.to_i
-p gets.split.take(n).collect(&:to_i)
-__END__
-3
-10 20 30
+## [B] Hammer
+## https://atcoder.jp/contests/abc270/tasks/abc270_b
+x, y, z = gets.split.map(&:to_i)
+if x < 0
+  x *= -1
+  y *= -1
+  z *= -1
+end
+if y > 0 && y < x
+  if y < z
+    puts(-1)
+  elsif z < 0
+    puts (-z) * 2 + x
+  else
+    puts x
+  end
+else
+  puts x
+end
